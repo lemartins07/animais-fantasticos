8ba85b50759dd3f1c17c7288bc566bd81156a027
Leandro Martins <leandro.martins.85@gmail.com>
2022-05-15 17:35:00 -0300

    scroll animation refatorado

---
+++ js/modules/scroll-animation.js
@@ -0,0 +1,26 @@
+export default class ScrollAnimation {
+  constructor(selector) {
+    this.sections = document.querySelectorAll(selector);
+    this.windowMetade = window.innerHeight * 0.6;
+
+    this.animaScroll = this.animaScroll.bind(this);
+  }
+
+  animaScroll() {
+    this.sections.forEach((section) => {
+      const sectionTop = section.getBoundingClientRect().top;
+      const isSectionVisible = (sectionTop - this.windowMetade) < 0;
+      if (isSectionVisible) {
+        section.classList.add('ativo');
+      } else if (section.classList.contains('ativo')) {
+        section.classList.remove('ativo');
+      }
+    });
+  }
+
+  init() {
+    this.animaScroll();
+    window.addEventListener('scroll', this.animaScroll);
+    return this;
+  }
+}
